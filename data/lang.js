0c24617b81c9404ef6ccfa083cc5807e019c9b06
Innuendo <k.ader@pc-team.de>
2023-12-08 16:34:31 +0100

    V 1.33c

--- data/lang.js
+++ data/lang.js
@@ -1,28 +1,7 @@
-/**
- * Add your language code to supportedLocales
- * e.g. if you want to add spanish
- *
- * supportedLocales = ["en", "de", "es"];
- *
- * copy /langauage/en.json to /languages/es.json
- * translate all words into spanish in each line after the colon
- * 
- * Do not change any word before the colon!
- *
- * {
- *   "SPEICHERN": " archivo",
- *   "SCHLIESSEN": " cerca",
- *   "MAISCHEPLAN": "plan de puré",
- *   "EINSTELLUNGEN": "ajustes".
- *   ...
- * }
- * 
- */
-
 // language configuration
-const supportedLocales = ["de", "en"];  // language files 
-const defaultLocale = "de";             // default language 
+const supportedLocales = ["de", "en"];  // language files
 
 // optional configuration 
+const defaultLocale = "de";         // dateTimePicker default language  (de, en, es, nl)
 const defaultTimeFormat = 'YMDHM';  // DateTimePicker Year Month Day Hour Minute
 const defaultFirstDayOfWeek = 1;    // DateTimePicker 0~6: Sunday~Saturday, e.g.: 1 (Monday), default: 0(Sunday)
