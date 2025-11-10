1140b7bdebd232c05b4b854f96f22ae394e11889
Innuendo <k.ader@pc-team.de>
2025-11-10 14:41:26 +0100

    V 1.59.5 beta

--- data/lang.js
+++
@@ -1,8 +0,0 @@
-// Web interface language configuration
-const supportedLocales = ["deutsch", "english", "norsk"];  // language files in JSON format
-const defaultLocale = "deutsch";
-
-// optional configuration dateTimePicker (timer based mash and brew). defaults: german language and date time format
-// const defaultLocaleDTP = "de";         // dateTimePicker default language  (de, en, es, nl)
-// const defaultTimeFormatDTP = 'YMDHM';  // DateTimePicker Year Month Day Hour Minute
-// const defaultFirstDayOfWeekDTP = 1;    // DateTimePicker 0~6: Sunday~Saturday, e.g.: 1 (Monday), default: 0(Sunday)
